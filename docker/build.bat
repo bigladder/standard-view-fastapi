dbd24697015525f3820081fbb4642fe2a44a1f03
kevin-moos <kevin.moos@bigladdersoftware.com>
2025-06-11 13:59:04 -0500

    Update Dockerfile and build script

--- docker/build.bat
+++ docker/build.bat
@@ -1,3 +1,13 @@
-docker build -t standard-view-fastapi --progress=plain --no-cache .
+echo off
+
+set IMAGE_NAME=standard-view-fastapi
+set CONTAINER_NAME=standard-view-fastapi
+set PORT=80
+
+docker stop %CONTAINER_NAME%
+docker rm %CONTAINER_NAME%
+docker rmi %IMAGE_NAME%
+docker build -t %IMAGE_NAME% --build-arg PORT=%PORT% --progress=plain --no-cache .
+docker run -d --name %CONTAINER_NAME% -p %PORT%:%PORT% %IMAGE_NAME%
 
 pause
--- docker/run.bat
+++
@@ -1,3 +0,0 @@
-docker run -d --name standard-view-fastapi -p 80:80 standard-view-fastapi
-
-pause
